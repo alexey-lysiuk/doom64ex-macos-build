3a79cca711c42386d589bbc00aeec4e708a89e7f
alexey.lysiuk <alexey.lysiuk@gmail.com>
2016-05-26 22:45:20 +0300

    + script to generate Xcode project using CMake

---
+++ prepare.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+CMAKE_EXE=`which cmake`
+
+if [ -z $CMAKE_EXE ]; then
+	CMAKE_EXE=/Applications/CMake.app/Contents/bin/cmake
+fi
+
+set -o errexit
+
+cd "`dirname \"$0\"`"
+
+TP_DIR=`pwd`
+SDL2_DIR=${TP_DIR}/SDL2
+SDL2_NET_DIR=${TP_DIR}/SDL2_net
+PNG_DIR=${TP_DIR}/png
+FLUIDSYNTH_DIR=${TP_DIR}/fluidsynth
+FLUIDSYNTH_LIB_PREFIX=${FLUIDSYNTH_DIR}/lib/lib
+
+if [ -e Doom64EX ]; then
+	cd Doom64EX
+	git pull
+	cd ..
+else
+	git clone https://github.com/svkaiser/Doom64EX.git
+fi
+
+if [ ! -e build ]; then
+	mkdir build
+fi
+
+cd build
+
+$CMAKE_EXE ../Doom64EX -GXcode \
+	-DCMAKE_OSX_ARCHITECTURES="i386;x86_64" \
+	-DCMAKE_OSX_DEPLOYMENT_TARGET="10.5" \
+	-DTESTING="NO" \
+	-DSDL2_INCLUDE_DIR="${SDL2_DIR}/include" \
+	-DSDL2_LIBRARY="${SDL2_DIR}/lib/libSDL2.a" \
+	-DSDL2_NET_INCLUDE_DIR="${SDL2_NET_DIR}/include" \
+	-DSDL2_NET_LIBRARY="${SDL2_NET_DIR}/lib/libSDL2_net.a" \
+	-DPNG_PNG_INCLUDE_DIR="${PNG_DIR}/include" \
+	-DPNG_LIBRARY_DEBUG="${PNG_DIR}/lib/libpng16.a" \
+	-DPNG_LIBRARY_RELEASE="${PNG_DIR}/lib/libpng16.a" \
+	-DFLUIDSYNTH_INCLUDE_DIR="${FLUIDSYNTH_DIR}/include" \
+	-DFLUIDSYNTH_LIBRARIES="${FLUIDSYNTH_LIB_PREFIX}fluidsynth.a;${FLUIDSYNTH_LIB_PREFIX}glib.a;${FLUIDSYNTH_LIB_PREFIX}intl.a" \
+	-DCMAKE_EXE_LINKER_FLAGS="-framework AudioUnit -framework Carbon -framework Cocoa -framework CoreAudio -framework CoreMIDI -framework CoreVideo -framework ForceFeedback"
