da8e7570bf052f0919d794d09eeae96fd868a342
alexey.lysiuk <alexey.lysiuk@gmail.com>
2016-05-29 12:39:56 +0300

    + create application bundle in deploy directory

--- build.sh
+++ build.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 
+# Configuration
+
+CONFIG=Release
+DEPLOY_DIR=../deploy
+SRC_BUNDLE=src/engine/${CONFIG}/doom64ex.app
+DST_BUNDLE=${DEPLOY_DIR}/Doom64EX.app
+DST_RES_DIR=${DST_BUNDLE}/Contents/Resources
+KEX_WAD=kex.wad
+
 set -o errexit
 
 cd "`dirname \"$0\"`"
-./prepare.sh "$1"
+./prepare.sh
 
 cd build
-xcodebuild -configuration Release
+xcodebuild -configuration "${CONFIG}" -target doom64ex -target kexwad
+
+if [ ! -e "${DST_RES_DIR}" ]; then
+	mkdir -p "${DST_RES_DIR}"
+fi
+
+rsync -av "${SRC_BUNDLE}/" "${DST_BUNDLE}"
+rsync -av kex.wad "${DST_RES_DIR}"
